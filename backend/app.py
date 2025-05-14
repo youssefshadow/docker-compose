5b505eb210bf930139a711e06c64350e6dbb25e5
unknown <youssef20ben@gmail.com>
2025-05-14 17:20:55 +0200

    Modifications backend : app.py, dockerfile et ajout des templates

--- backend/app.py
+++ backend/app.py
@@ -1,48 +1,10 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    html = """
-    <!DOCTYPE html>
-    <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>DevOps Web</title>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                background: #f4f4f4;
-                color: #333;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-                margin: 0;
-            }
-            .container {
-                background: white;
-                padding: 40px;
-                border-radius: 10px;
-                box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-                text-align: center;
-            }
-            h1 {
-                color: #007ACC;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <h1>👋 Hi Team Bienvenue, option DevOps !</h1>
-            <p>Cette page est servie par une application Flask dans un conteneur Docker.</p>
-        </div>
-    </body>
-    </html>
-    """
-    return render_template_string(html)
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
